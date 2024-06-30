19fc3b8b3bb77669a9cbc5521a8fe223ef25bcbf
HimangshuDe <dehimangshu03@gmail.com>
2024-06-30 19:55:21 +0530

    Added strings-n-template literals

---
+++ strings-n-template-literals.js
@@ -0,0 +1,24 @@
+const firstName = "Himangshu";
+const job = 'teacher';
+const birthYear = 2003;
+const year = 2037
+
+const himangshu = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + "!";
+
+console.log(himangshu);
+
+const himangshuNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+
+console.log(himangshuNew);
+
+console.log("just A regular string!");
+
+console.log("string with \n\
+multiple \n\
+lines");
+
+console.log(`This is my first name ${firstName}`);
+console.log(`String with
+    multiple
+    lines
+    using backticks \``);
