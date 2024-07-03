8a2ca87c9d3ced78133a1fbf9b5e308f5f866e51
HimangshuDe <dehimangshu03@gmail.com>
2024-07-03 10:48:40 +0530

    Added Logical Operators in js

---
+++ logical-operators.js
@@ -0,0 +1,23 @@
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+
+// if (hasDriversLicense && hasGoodVision) {
+//     console.log("You are able to drive");
+// } else {
+//     console.log("Someone else should drive");
+// }
+
+const isTired = false; // C
+
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+    console.log("You are able to drive");
+} else {
+    console.log("Someone else should drive");
+}
