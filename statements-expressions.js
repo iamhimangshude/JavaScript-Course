323b8c0d018e948b750c059d49b0f6ed692199b1
HimangshuDe <dehimangshu03@gmail.com>
2024-07-03 11:32:19 +0530

    added statements and expressions in js

---
+++ statements-expressions.js
@@ -0,0 +1,15 @@
+3 + 4;
+1991
+true && false && !false
+
+if (23 > 10) {
+    const str = '23 is bigger'; // statements
+}
+
+console.log(`I am ${2037 - 2003} years old`); // expressions
+
+/*
+Statements end with semi colon
+expressions consists of operators and keywords 
+including values, variables leading to computing a value.
+*/
