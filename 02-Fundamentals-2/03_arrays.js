b6a18c52c4948fe7f7e981c877c0af83aee56c7e
HimangshuDe <dehimangshu03@gmail.com>
2024-07-05 11:14:56 +0530

    Added arrays in js

--- 02-Fundamentals-2/03_arrays.js
+++ 02-Fundamentals-2/03_arrays.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 const friend1 = "Michael";
 const friend2 = "Steven";
 const friend3 = "Peter";
@@ -46,3 +46,63 @@ console.log(age1, age2, age3);
 
 const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
 console.log(ages);
+
+
+///////////////////////////////////////////
+
+// Basic Array Methods
+const friends = ["Michael", "Steven", "Peter"];
+const newLength = friends.push("Jay");
+
+// Push
+// Appends the element at the end of an array and returns new length
+
+console.log(friends);
+console.log(newLength);
+
+
+//unshift
+//Inserts the element at the start of an array and returns new length
+
+friends.unshift('John');
+console.log(friends);
+
+
+// Remove elements from...
+friends.pop(); // last
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+
+friends.shift();// first
+console.log(friends);
+
+
+// Getting the positions of elements in an array
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Michale'));
+
+//`indexOf` returns the position of an element, if not 
+//found returns -1.
+
+
+// ----------------
+// checking whether an element is present in an array 
+// or not
+
+//`includes` checks for a given value (uses ===) and
+//returns a boolean value.
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes('23'));
+console.log(friends.includes(23));
+
+if (friends.includes('Steven')) {
+    console.log("You have a friend called Steven");
+}
+/////////////////////////////////////////////
+
+*/
