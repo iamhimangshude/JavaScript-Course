b8b795f95dd82be881d18b2dd6ef19c4530dfaa5
HimangshuDe <dehimangshu03@gmail.com>
2024-07-07 07:58:48 +0530

    Updated 04_objects.js

--- 02-Fundamentals-2/04_objects.js
+++ 02-Fundamentals-2/04_objects.js
@@ -50,31 +50,31 @@ console.log(`${hd.firstName} has ${hd.friends.length} friends, and his best frie
 ////////////////////////////////////////////
 
 // Object Methods
-const hd = {
-    firstName: 'Himangshu',
-    lastName: 'De',
-    birthYear: 2003,
-    job: 'teacher',
-    friends: ['Michael', 'Peter', 'Steven'],
-    hasDriversLicense: true,
+// const hd = {
+//     firstName: 'Himangshu',
+//     lastName: 'De',
+//     birthYear: 2003,
+//     job: 'teacher',
+//     friends: ['Michael', 'Peter', 'Steven'],
+//     hasDriversLicense: true,
 
-    calcAge: function () {
-        this.age = 2037 - this.birthYear;
-        return this.age;
-    },
-    getSummary: function () {
-        return `${this.firstName} is a ${this.calcAge()} year old ${this.job}, and he has ${this.hasDriversLicense ? "a" : "no"} driver's license`
-    }
-}
+//     calcAge: function () {
+//         this.age = 2037 - this.birthYear;
+//         return this.age;
+//     },
+//     getSummary: function () {
+//         return `${this.firstName} is a ${this.calcAge()} year old ${this.job}, and he has ${this.hasDriversLicense ? "a" : "no"} driver's license`
+//     }
+// }
 
-console.log(hd.calcAge());
-console.log(hd.age);
-console.log(hd.age);
-console.log(hd.age);
+// console.log(hd.calcAge());
+// console.log(hd.age);
+// console.log(hd.age);
+// console.log(hd.age);
 // console.log(hd['calcAge']());
 
 //Challenge
 // "Himangshu is a 34 year old teacher, and he has a/no driver's license"
 
 
-console.log(hd.getSummary());
+// console.log(hd.getSummary());
