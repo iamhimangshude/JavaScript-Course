5600abbb89221b505b4c84eca483a0d0e74f5a53
HimangshuDe <dehimangshu03@gmail.com>
2024-07-07 07:57:51 +0530

    Added objects in js

---
+++ 02-Fundamentals-2/04_objects.js
@@ -0,0 +1,80 @@
+'use strict';
+/*
+const hdArray = [
+    'Himangshu',
+    'De',
+    2037 - 2003,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+];
+
+
+const hd = {
+    firstName: 'Himangshu',
+    lastName: 'De',
+    age: 2037 - 2003,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven'],
+}
+
+
+console.log(hd);
+
+console.log(hd.lastName);
+console.log(hd['lastName']);
+
+const nameKey = 'Name';
+console.log(hd['first' + nameKey]);
+console.log(hd['last' + nameKey]);
+
+// console.log(hd.'last' + nameKey);
+
+const interestedIn = prompt("What do you want to know about Himangshu? Choose between firstName, lastName, age, job and friends");
+
+
+if (hd[interestedIn]) {
+    console.log(hd[interestedIn]);
+} else {
+    console.log('Wrong request! Choose between firstName, lastName, age, job and friends')
+}
+
+hd.location = 'India'
+hd['twitter'] = '@himangshuishere'
+console.log(hd);
+
+// Challenge
+// "Himangshu has 3 friends, and his best friend is called Michael"
+
+console.log(`${hd.firstName} has ${hd.friends.length} friends, and his best friend is called ${hd.friends[0]}`);
+*/
+////////////////////////////////////////////
+
+// Object Methods
+const hd = {
+    firstName: 'Himangshu',
+    lastName: 'De',
+    birthYear: 2003,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven'],
+    hasDriversLicense: true,
+
+    calcAge: function () {
+        this.age = 2037 - this.birthYear;
+        return this.age;
+    },
+    getSummary: function () {
+        return `${this.firstName} is a ${this.calcAge()} year old ${this.job}, and he has ${this.hasDriversLicense ? "a" : "no"} driver's license`
+    }
+}
+
+console.log(hd.calcAge());
+console.log(hd.age);
+console.log(hd.age);
+console.log(hd.age);
+// console.log(hd['calcAge']());
+
+//Challenge
+// "Himangshu is a 34 year old teacher, and he has a/no driver's license"
+
+
+console.log(hd.getSummary());
