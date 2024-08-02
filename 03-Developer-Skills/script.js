177a8175532bb990c2811dab2f63ad6e40b0174b
HimangshuDe <dehimangshu03@gmail.com>
2024-08-02 07:59:36 +0530

    🚀 Updated script.js

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,6 +1,9 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+/*
+///////////////////////////////////////////////
+Using Google, Stack Overflow and MDN
 // PROBLEM 1:
 // We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
 
@@ -67,3 +70,98 @@ const calcTempAmplitudeNew = function (t1, t2) {
 
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 console.log(amplitudeNew);
+
+////////////////////////////////////////////
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+
+    // C) FIX
+    // value: Number(prompt('Degrees Celsius: ')),
+    value: 10,
+  };
+
+  // B) FIND
+  console.table(measurement);
+
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+// A) IDENTIFY
+console.log(measureKelvin());
+
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+
+  console.log(temps);
+
+  debugger; // adds a breakpoint in the code execution process.
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = temps[i];
+    if (curTemp < min) min = temps[i];
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+console.log(amplitudeBug);
+
+
+////////////////////////////////////
+// Coding Challenge #1
+
+/* Given an array of forecasted maximum temperatures,
+the thermometer displays a string with these
+temperatures.
+
+Example: [17, 21, 23] will print "... 17°C in 1 days .
+.. 21℃ in 2 days ... 23℃ in 3 days ..."
+
+Create a function 'printForecast' which takes in an 
+array 'arr' and logs a string like the above to the
+console.
+
+Use the problem-solving framework: Understand 
+the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+
+// 1) Unerstanding the problem
+// - Array transformed to string, separated by ...
+// - What is the X days? Answer: index + 1
+
+// 2) Breaking up into sub-problem
+// - Transform array into string
+// - Transform each element to string with ℃
+// - Strings needs to contain day (index + 1)
+// - Add ... between elements and start and at end of string.
+
+
+
+const printForecast = function (arr) {
+  let finalString = '... ';
+  for (let temp = 0; temp < arr.length; temp++) {
+    const outputString = ` ${arr[temp]}℃ in ${temp + 1} days ...`;
+    finalString += outputString;
+  }
+  console.log(finalString);
+};
+
+printForecast([17, 21, 23]);
+printForecast([12, 5, -5, 0, 4]);
+*/
