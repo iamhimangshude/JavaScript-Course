dcb1a07987154d3b1da890495d74114457984013
HimangshuDe <dehimangshu03@gmail.com>
2024-11-14 11:43:30 +0530

    🏁 Completed Functions in JS

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -395,3 +395,48 @@ const boardPassengers = function (n, wait) {
 const perGroup = 1000;
 boardPassengers(180, 3);
 */
+///////////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+
+*/
+
+// My Answer:
+
+// Explanation:
+/* 
+I have created a double closure so that whenever we click the body element the h1's color changes to blue and after a second it is changed to red again! Because it stated that `each time the BODY element is clicked`
+
+
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+    setTimeout(function () {
+      header.style.color = 'red';
+    }, 1000);
+  });
+})();
+*/
+// Correct Answer:
+/* 
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
+*/
