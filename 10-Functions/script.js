bbe77e6c7d39e8e099e63631dc5c4155e35402ff
HimangshuDe <dehimangshu03@gmail.com>
2024-11-14 11:29:22 +0530

    🚀 Completed upto Closures in JS!

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -301,4 +301,97 @@ poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
 poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
 poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
 
- */
+*/
+
+//////////////////////////////////////////////////////
+// Immediately Invoked Function Expressions (IIFE)
+/* 
+const runOnce = function () {
+  console.log('This will never run again!');
+};
+
+runOnce();
+
+// IIFE
+(function () {
+  console.log('This will never run again!');
+  const isPrivate = 23;
+})();
+
+// console.log(isPrivate);
+
+(() => console.log('This will also never run again!'))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+
+// console.log(isPrivate); // <- Throws error! because isPrivate is locally scoped!
+console.log(notPrivate);
+
+
+//////////////////////////////////////////////////////
+// Closures
+
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker); // This logs the object that is returned by the secureBooking function
+
+/////////////////////////////////////////////////////
+// More closure examples
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Reassigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  // const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
+*/
